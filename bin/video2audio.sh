d9f3d62b093e0e76d1edb86db0e396a92159ca84
Jaromir Konecny <jaromir.konecny@ysoft.com>
2016-09-06 09:00:54 +0200

    scripts/video2audio: Automation of multiple conversion

--- bin/video2audio.sh
+++ bin/video2audio.sh
@@ -1,3 +1,13 @@
+#!/bin/sh
+
+for FILE in "$@"; do
+  target=$(basename "$FILE")
+  target=${FILE%.*}.mp3
+  echo "Converting \"$FILE\" to \"$target\" ... "
+  ffmpeg -i "$FILE" "$target"
+done
+
+exit 0
 ffmpeg -i filename.mp4 filename.mp3
 ffmpeg -i video.mp4 -b:a 192K -vn music.mp3
 ffmpeg -i k.mp4 -q:a 0 -map a k.mp3
