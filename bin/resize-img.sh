c3601987343653ad8e4814312709425873102669
Jaromir Konecny <jaromir.konecny@ysoft.com>
2017-01-03 15:41:14 +0100

    scripts/resize-video: Dynamicke dopocitavani pomeru stran

--- bin/resize-img.sh
+++ bin/resize-img.sh
@@ -1,21 +1,40 @@
 #!/bin/sh
 
-ext=${1:-[Jj][Pp][Gg]}
-size=${2:-"800x600"}
-dir=convert
+# Test if commands exit
+type > /dev/null identify && type > /dev/null convert
 
-if [ -z "$ext" ]; then
-  echo Pouziti: `basename $0` {pripona} [rozliseni - implicitni 800x600]
+if [ $? != 0 ]; then
+  echo "Install imagemagic !"
   exit 1
 fi
 
-if [ ! -d $dir ]; then
-  mkdir $dir;
+SCRIPT_PATH=$(realpath $0)
+SCRIPT_DIR=${SCRIPT_PATH%/*}
+SCRIPT_NAME=${SCRIPT_PATH##*/}
+
+FILE="$1"
+FILE_PATH="$(realpath "$FILE")"
+WIDTH_NEW=800
+
+if [ -z "$FILE" ]; then
+  echo "Usage: SCRIPT_NAME {file}"
+  exit 1
 fi
 
-for i in `ls -1 *.$ext`; do
-  echo $i
-#  convert -resize $2 -rotate 90 $i $dir/$i;
-#  convert -resize $2 $i $dir/$i;
-  convert $i -resize $size $dir/$i;
-done
+RESOLUTION_ORIG=$(identify -format '%wx%h' "$FILE_PATH")
+WIDTH_ORIG=$(echo $RESOLUTION_ORIG | awk -F x '{print $1}')
+HEIGHT_ORIG=$(echo $RESOLUTION_ORIG | awk -F x '{print $2}')
+
+if (( $WIDTH_ORIG > $WIDTH_NEW )); then
+  SCALE=$(echo "$WIDTH_ORIG/$WIDTH_NEW" | bc -l)
+  #SCALE=$(($WIDTH_ORIG / $WIDTH_NEW))
+  HEIGHT_NEW=$(echo "$HEIGHT_ORIG/$SCALE" | bc -l)
+  HEIGHT_NEW=$(echo $HEIGHT_NEW/1 | bc)
+  #HEIGHT_NEW=$(($HEIGHT_ORIG/$SCALE))
+
+  FILE_DIR=${FILE_PATH%/*}
+  FILE_NAME=${FILE_PATH##*/}
+  FILE_SUFF=${FILE_NAME##*.}
+  FILE_SHORT_NAME=${FILE_NAME%%.*}
+  convert "$FILE_PATH" -resize ${WIDTH_NEW}x${HEIGHT_NEW} "${FILE_DIR}/${FILE_SHORT_NAME}_small.${FILE_SUFF}"
+fi
